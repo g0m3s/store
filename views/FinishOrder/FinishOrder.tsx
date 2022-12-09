700c48db708b4d74ccef8c05e6f5d7875d1a0efc
g0m3s <gom3s.g@gmail.com>
2022-12-09 17:45:58 -0300

    feat: finish order form validation && integration

--- views/FinishOrder/FinishOrder.tsx
+++ views/FinishOrder/FinishOrder.tsx
@@ -1,17 +1,26 @@
+import * as yup from 'yup'
+import { useMemo } from 'react'
+import { useForm } from 'react-hook-form'
 import { Stack, Typography } from '@mui/material'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { getCartItems } from '../../utils/localStorage'
 import { useIsDarkMode } from '../../utils/useIsDarkMode'
 import { LocalShippingOutlined, SellOutlined } from '@mui/icons-material'
-import { useMemo } from 'react'
 
-interface InputProps {
-  placeholder: string
-  type: 'email' | 'number' | 'tel' | 'text'
+interface FormProps {
+  taxId: string
+  street: string
+  fullName: string
+  district: string
+  phoneNumber: string
+  zipCode: string | number
+  houseNumber: string | number
+
 }
 
 export const FinishOrder: React.FC = () => {
-  const isDarkMode = useIsDarkMode()
   const cartItems = getCartItems()
+  const isDarkMode = useIsDarkMode()
 
   const formattedProducts = useMemo(() => {
     const newProducts = cartItems.map(product => ({
@@ -25,28 +34,64 @@ export const FinishOrder: React.FC = () => {
     }
   }, [cartItems])
 
-  // "statement_descriptor": "MEUNEGOCIO"
+  const validationSchema: yup.SchemaOf<FormProps> = yup.object().shape({
+    fullName: yup.string().required('Campo obrigatório'),
+    street: yup.string().required('Campo obrigatório'),
+    district: yup.string().required('Campo obrigatório'),
+    zipCode: yup.string().min(8, 'CEP precisa ter 8 dígitos').required('Campo obrigatório'),
+    houseNumber: yup.string().required('Campo obrigatório'),
+    taxId: yup.string().min(8, 'CEP precisa ter 8 dígitos').required('Campo obrigatório'),
+    phoneNumber: yup.string().required('Campo obrigatório').matches(/^\d{10,14}$/, 'Celular inválido'),
 
-  const generatePayment = () => {
-    const payer = {
-      name: "Joao",
-      surname: "Silva",
-      email: "user@email.com",
-      date_created: "2015-06-02T12:58:41.425-04:00",
+  })
+
+  const defaultValues: FormProps = {
+    fullName: '',
+    street: '',
+    district: '',
+    zipCode: '',
+    houseNumber: '',
+    taxId: '',
+    phoneNumber: '',
+  }
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<FormProps>({
+    defaultValues,
+    resolver: yupResolver(validationSchema),
+  })
+
+  const inputStyle = {
+    padding: 16,
+    fontSize: 16,
+    width: '100%',
+    color: 'black',
+    borderRadius: 6,
+    outline: 'none',
+    // appearance: 'none',
+    border: `${isDarkMode ? '1.5px' : '1px'} solid transparent`,
+    background: 'linear-gradient(white, white) padding-box, linear-gradient(80.42deg, #9A00FF 7.33%, #7241FF 51.42%, orange 92.84%) border-box',
+  }
+
+  const onSubmit = (data: FormProps) => {
+    const userInfos = {
+      name: data.fullName,
+      surname: '',
+      email: '',
+      date_created: new Date().toDateString(),
       phone: {
-        area_code: "11",
-        number: "4444-4444"
+        area_code: data.phoneNumber.slice(0, 2),
+        number: data.phoneNumber.slice(2, 11)
       },
 
       identification: {
         type: "CPF",
-        number: "19119119100"
+        number: data.taxId
       },
 
       address: {
-        street_name: "Street",
-        street_number: "123",
-        zip_code: "06233200"
+        street_name: data.street,
+        street_number: data.houseNumber,
+        zip_code: data.zipCode
       }
     }
 
@@ -58,6 +103,7 @@ export const FinishOrder: React.FC = () => {
       },
       body: JSON.stringify({
         ...formattedProducts,
+        payer: userInfos,
         back_urls: {
           success: 'http://localhost:3000',
           failure: 'http:/localhost:3000',
@@ -74,73 +120,80 @@ export const FinishOrder: React.FC = () => {
       })
   }
 
-  const Input: React.FC<InputProps> = (props) => (
-    <input
-      {...props}
-      style={{
-        padding: 16,
-        fontSize: 16,
-        width: '100%',
-        color: 'black',
-        borderRadius: 6,
-        outline: 'none',
-        appearance: 'none',
-        border: `${isDarkMode ? '1.5px' : '1px'} solid transparent`,
-        background: 'linear-gradient(white, white) padding-box, linear-gradient(80.42deg, #9A00FF 7.33%, #7241FF 51.42%, orange 92.84%) border-box',
-      }}
-    />
-  )
-
   return (
-    <Stack height='85vh' justifyContent='space-between' mt='8vh' pb={8}>
+    <Stack height='85vh' mt='8vh' pb={8}>
       <Stack>
-        <Stack my={3} width='100%' direction='row' alignItems='center' justifyContent='space-between'>
+        <Stack justifyContent='space-between' my={3} width='100%' direction='row' alignItems='center'>
           <Typography textAlign='center' variant='h4'><b>Informações de envio</b></Typography>
           <LocalShippingOutlined />
         </Stack>
-        <Stack gap={2}>
-          <Input type='text' placeholder='Nome Completo' />
-          <Input type='text' placeholder='Rua' />
-          <Input type='text' placeholder='Bairro' />
-          <Stack gap={1} direction='row' width='100%' justifyContent='space-between'>
-            <Input type='text' placeholder='Número' />
-            <Input type='number' placeholder='CEP' />
+        <Stack component='form' onSubmit={handleSubmit(onSubmit)} gap={2}>
+          <Stack>
+            {errors.fullName?.message && (<Typography fontSize={12} color='red'>{errors.fullName?.message}</Typography>)}
+            <input style={{ ...inputStyle }} {...register('fullName')} type='text' placeholder='Nome Completo' />
           </Stack>
-          <Input type='number' placeholder='CPF' />
-          <Input type='tel' placeholder='Número de telefone (WhatsApp)' />
+          <Stack>
+            {errors.street?.message && (<Typography fontSize={12} color='red'>{errors.street?.message}</Typography>)}
+            <input style={{ ...inputStyle }} {...register('street')} type='text' placeholder='Rua' />
+          </Stack>
+          <Stack>
+            {errors.district?.message && (<Typography fontSize={12} color='red'>{errors.district?.message}</Typography>)}
+            <input style={{ ...inputStyle }} {...register('district')} type='text' placeholder='Bairro' />
+          </Stack>
+          <Stack gap={1} direction='row' width='100%' justifyContent='space-between'>
+            <Stack>
+              {errors.houseNumber?.message && (<Typography fontSize={12} color='red'>{errors.houseNumber?.message}</Typography>)}
+              <input style={{ ...inputStyle }} {...register('houseNumber')} type='number' placeholder='Número' />
+            </Stack>
+            <Stack>
+              {errors.zipCode?.message && (<Typography fontSize={12} color='red'>{errors.zipCode?.message}</Typography>)}
+              <input style={{ ...inputStyle }} {...register('zipCode')} type='number' placeholder='CEP' />
+            </Stack>
+          </Stack>
+          <Stack>
+            {errors.taxId?.message && (<Typography fontSize={12} color='red'>{errors.taxId?.message}</Typography>)}
+            <input style={{ ...inputStyle }} {...register('taxId')} type='number' placeholder='CPF' />
+          </Stack>
+          <Stack>
+            {errors.phoneNumber?.message && (<Typography fontSize={12} color='red'>{errors.phoneNumber?.message}</Typography>)}
+            <input style={{ ...inputStyle }} {...register('phoneNumber')} type='tel' placeholder='Número de telefone (WhatsApp)' />
+          </Stack>
+
+          <Stack
+            py={2}
+            mt={10}
+            px={1.5}
+            type='submit'
+            direction='row'
+            borderRadius={2}
+            component='button'
+            alignItems='center'
+            position='relative'
+            justifyContent='space-between'
+            bgcolor={isDarkMode ? 'white' : 'black'}
+            border={`${isDarkMode ? '1.5px' : '1px'} solid transparent`}
+            sx={{
+              background: `linear-gradient(${isDarkMode ? 'white, white' : 'black, black'}) padding-box, linear-gradient(80.42deg, #9A00FF 7.33%, #7241FF 51.42%, orange 92.84%) border-box`,
+            }}
+          >
+            <Typography
+              variant='button'
+              sx={{
+                backgroundClip: 'text',
+                mozBackgroundClip: 'text',
+                webkitBackgroundClip: 'text',
+                color: isDarkMode ? 'transparent' : 'white',
+                backgroundImage: 'linear-gradient(80.42deg, #9A00FF 7.33%, #7241FF 51.42%, orange 92.84%)',
+              }}
+            >
+              Finalizar compra
+            </Typography>
+            <SellOutlined sx={{ color: isDarkMode ? 'orange' : 'white', }} />
+          </Stack>
         </Stack>
       </Stack>
 
-      <Stack
-        py={2}
-        px={1.5}
-        mt={1.5}
-        direction='row'
-        borderRadius={2}
-        alignItems='center'
-        position='relative'
-        justifyContent='space-between'
-        onClick={() => generatePayment()}
-        bgcolor={isDarkMode ? 'white' : 'black'}
-        border={`${isDarkMode ? '1.5px' : '1px'} solid transparent`}
-        sx={{
-          background: `linear-gradient(${isDarkMode ? 'white, white' : 'black, black'}) padding-box, linear-gradient(80.42deg, #9A00FF 7.33%, #7241FF 51.42%, orange 92.84%) border-box`,
-        }}
-      >
-        <Typography
-          variant='button'
-          sx={{
-            backgroundClip: 'text',
-            mozBackgroundClip: 'text',
-            webkitBackgroundClip: 'text',
-            color: isDarkMode ? 'transparent' : 'white',
-            backgroundImage: 'linear-gradient(80.42deg, #9A00FF 7.33%, #7241FF 51.42%, orange 92.84%)',
-          }}
-        >
-          Finalizar compra
-        </Typography>
-        <SellOutlined sx={{ color: isDarkMode ? 'orange' : 'white', }} />
-      </Stack>
+
 
       <Stack position='fixed' bottom='50px'>
         <Typography fontSize={12} variant='body2'>* O tempo estimado para entrega é de 20 dias</Typography>
