f1e6dda50a328dc9ef6cff42ba972f21a2a90b4b
g0m3s <gom3s.g@gmail.com>
2022-12-13 18:32:38 -0300

    feat: animation on add item to cart

--- components/AddProductModal/AddProductModal.tsx
+++ components/AddProductModal/AddProductModal.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
 import 'swiper/css'
 import Lottie from 'react-lottie'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Product } from '../../utils/mockedData'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { SetCurrentScreen } from '../../types/utils'
 import { Dialog, Stack, Typography } from '@mui/material'
 import { setNewCartItem } from '../../utils/localStorage'
 import { useIsDarkMode } from '../../utils/useIsDarkMode'
-import addToBagAnimation from '../../public/animations/addToBag.json'
+import successAnimation from '../../public/animations/success.json'
 import { SellOutlined, ShoppingBagOutlined } from '@mui/icons-material'
 
 interface AddProductModalProps extends SetCurrentScreen {
@@ -21,19 +21,14 @@ export const AddProductModal: React.FC<AddProductModalProps> = ({ isOpen, onClos
   const isDarkMode = useIsDarkMode()
   const [selectedSize, setSelectedSize] = useState<string>()
   const [selectedColor, setSelectedColor] = useState<string>()
-  const [addItemToBag, setAddItemToBag] = useState<boolean>(false)
   const [haveSizeError, setHaveSizeError] = useState<boolean>(false)
   const [haveColorError, setHaveColorError] = useState<boolean>(false)
-
-  const onCloseModal = () => {
-    setAddItemToBag(false)
-    onClose()
-  }
+  const [showSuccessAnimation, setShowSuccessAnimation] = useState(false);
 
   const defaultOptions = {
-    loop: false,
-    autoplay: false,
-    animationData: addToBagAnimation,
+    loop: true,
+    autoplay: true,
+    animationData: successAnimation,
     rendererSettings: {
       preserveAspectRatio: 'xMidYMid slice'
     },
@@ -60,7 +55,13 @@ export const AddProductModal: React.FC<AddProductModalProps> = ({ isOpen, onClos
         selectedSize: selectedSize,
         selectedColor: selectedColor,
       })
-      onClose()
+      setShowSuccessAnimation(true)
+      setTimeout(() => {
+        setSelectedSize('')
+        setSelectedColor('')
+        setShowSuccessAnimation(false)
+        onClose()
+      }, 2500)
     }
   }
   const finishOrder = () => {
@@ -81,19 +82,11 @@ export const AddProductModal: React.FC<AddProductModalProps> = ({ isOpen, onClos
     }
   }
 
-  useEffect(() => {
-    () => {
-      setSelectedSize('')
-      setSelectedColor('')
-      setAddItemToBag(false)
-    }
-  })
-
   return (
     <Dialog
       scroll='body'
       open={isOpen}
-      onClose={onCloseModal}
+      onClose={onClose}
       PaperProps={{
         sx: {
           py: 3,
@@ -112,190 +105,187 @@ export const AddProductModal: React.FC<AddProductModalProps> = ({ isOpen, onClos
         }
       }}
     >
-      <Stack position='relative' alignItems='center' justifyContent='center'>
-        <Stack mb={1} borderRadius={10} bgcolor='black' width={40} height={8} />
-        <Stack px={2} height='100%' width='100%'>
-          <Typography mb={2} textAlign='center' variant='h4'><b>{product?.title}</b></Typography>
-
-          <Stack maxWidth='100vw'>
-            {product?.imgs && (
-              <Swiper
-                spaceBetween={30}
-                slidesPerView={1.2}
-                style={{ maxWidth: '95vw' }}
-              >
-                {product?.imgs.map((imgPath, key) => (
-                  <SwiperSlide key={key}>
-                    <img
-                      style={{
-                        width: '100%',
-                        height: '300px',
-                        borderRadius: 10,
-                        objectFit: 'contain',
-                        boxShadow: '0px 0px 10px rgba(0,0,0,.1)'
-                      }}
-                      src={imgPath}
-                      alt='product image'
-                    />
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            )}
-          </Stack>
-
-          <Typography variant='body2' mt={.5}>{product?.description}</Typography>
-          <Typography
-            mt={.5}
-            fontSize='18px'
-            textAlign='right'
-            sx={{ textShadow: '0px 0px 3px rgba(0,0,0,.25)' }}
-          >
-            <b>R$ {product?.price.toFixed(2)}</b>
-          </Typography>
-
-          <Stack>
-            {product?.color && (
-              <>
-                <Stack direction='row' alignItems='center'>
-                  <Typography mr={.5} variant='body2'>Cor:</Typography>
-                  {product.color.map(color => {
-                    const isSelected = selectedColor === color
-                    return (
-                      <Stack
-                        p={.5}
-                        mx={.5}
-                        key={color}
-                        minWidth='50px'
-                        borderRadius={1}
-                        alignItems='center'
-                        justifyContent='center'
-                        sx={{ cursor: 'pointer' }}
-                        border='1px solid rgba(0,0,0,.1)'
-                        color={selectedItemsColor(isSelected)}
-                        bgcolor={selectedItemsBgColor(isSelected)}
-                        // onClick={() => setSelectedColor(color)}
-                        onClick={() => {
-                          if (isSelected) {
-                            return
-                          }
-                          setSelectedColor(color)
-                        }}
-                      >
-                        <Typography fontSize='13px' variant='body2'>{color}</Typography>
-                      </Stack>
-                    )
-                  })}
-                </Stack>
-                {haveColorError && <Typography fontSize={12} color='red' variant='body2'>Obrigatório</Typography>}
-              </>
-            )}
-            {product?.size && (
-              <>
-                <Stack mt={1} direction='row' alignItems='center'>
-                  <Typography mr={.5} variant='body2'>Tamanho:</Typography>
-                  {product.size.map(size => {
-                    const isSelected = selectedSize === size
-                    return (
-                      <Stack
-                        p={.5}
-                        mx={.5}
-                        key={size}
-                        minWidth='50px'
-                        borderRadius={1}
-                        alignItems='center'
-                        justifyContent='center'
-                        sx={{ cursor: 'pointer' }}
-                        border='1px solid rgba(0,0,0,.1)'
-                        color={selectedItemsColor(isSelected)}
-                        bgcolor={selectedItemsBgColor(isSelected)}
-                        onClick={() => {
-                          if (isSelected) {
-                            return
-                          }
-                          setSelectedSize(size)
-                        }}
-                      >
-                        <Typography fontSize='13px' variant='body2'>{size}</Typography>
-                      </Stack>
-                    )
-                  })}
-                </Stack>
-                {haveSizeError && <Typography fontSize={12} color='red' variant='body2'>Obrigatório</Typography>}
-              </>
-            )}
-          </Stack>
-
-          <Stack
-            mt={2}
-            p={1.5}
-            direction='row'
-            borderRadius={2}
-            alignItems='center'
-            onClick={() => addToCart()}
-            justifyContent='space-between'
-            sx={{ opacity: .6, cursor: 'pointer' }}
-          >
-            <Typography variant='button'>Adicionar ao carrinho</Typography>
-            <ShoppingBagOutlined />
-          </Stack>
-
-          <Stack
-            py={2}
-            px={1.5}
-            mt={1.5}
-            direction='row'
-            bgcolor='white'
-            borderRadius={2}
-            alignItems='center'
-            position='relative'
-            justifyContent='space-between'
-            border={`${isDarkMode ? '1.5px' : '1px'} solid transparent`}
-            sx={{
-              background: 'linear-gradient(white, white) padding-box, linear-gradient(80.42deg, #9A00FF 7.33%, #7241FF 51.42%, orange 92.84%) border-box',
-            }}
-            onClick={() => finishOrder()}
-          >
-            <Typography
-              variant='button'
-              sx={{
-                color: 'transparent',
-                backgroundClip: 'text',
-                mozBackgroundClip: 'text',
-                webkitBackgroundClip: 'text',
-                backgroundImage: 'linear-gradient(80.42deg, #9A00FF 7.33%, #7241FF 51.42%, orange 92.84%)',
-              }}
-            >
-              Comprar agora
-            </Typography>
-            <SellOutlined sx={{ color: 'orange' }} />
-          </Stack>
-        </Stack>
+      <Stack
+        width='100vw'
+        alignItems='center'
+        textAlign='center'
+        justifyContent='center'
+        display={showSuccessAnimation ? 'block' : 'none'}
+      >
+        <Lottie
+          width={350}
+          height={200}
+          options={defaultOptions}
+          isStopped={!showSuccessAnimation}
+        />
+        <Typography
+          fontSize={20}
+          variant='button'
+          sx={{ textShadow: '0px 0px 5px rgba(0,0,0,.15)' }}
+        >
+          <b>Adicionado com sucesso</b>
+        </Typography>
       </Stack>
 
-      {
-        addItemToBag && (
-          <Stack
-            sx={{
-              top: 0,
-              zIndex: 999,
-              width: '100%',
-              height: '100%',
-              bgcolor: 'white',
-              position: 'absolute',
-              alignItems: 'center',
-              justifyContent: 'center',
-              opacity: addItemToBag ? 1 : 0
-            }}>
-            <Lottie
-              width={350}
-              height={350}
-              speed={3000}
-              isPaused={!addItemToBag}
-              options={defaultOptions}
-            />
+      {!showSuccessAnimation && (
+        <Stack position='relative' alignItems='center' justifyContent='center'>
+          <Stack px={2} height='100%' width='100%'>
+            <Typography mb={2} textAlign='center' variant='h4'><b>{product?.title}</b></Typography>
+
+            <Stack maxWidth='100vw'>
+              {product?.imgs && (
+                <Swiper
+                  spaceBetween={30}
+                  slidesPerView={1.2}
+                  style={{ maxWidth: '95vw' }}
+                >
+                  {product?.imgs.map((imgPath, key) => (
+                    <SwiperSlide key={key}>
+                      <img
+                        style={{
+                          width: '100%',
+                          height: '300px',
+                          borderRadius: 10,
+                          objectFit: 'contain',
+                          boxShadow: '0px 0px 10px rgba(0,0,0,.1)'
+                        }}
+                        src={imgPath}
+                        alt='product image'
+                      />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              )}
+            </Stack>
+
+            <Typography variant='body2' mt={.5}>{product?.description}</Typography>
+            <Typography
+              mt={.5}
+              fontSize='18px'
+              textAlign='right'
+              sx={{ textShadow: '0px 0px 3px rgba(0,0,0,.25)' }}
+            >
+              <b>R$ {product?.price.toFixed(2)}</b>
+            </Typography>
+
+            <Stack>
+              {product?.color && (
+                <>
+                  <Stack direction='row' alignItems='center'>
+                    <Typography mr={.5} variant='body2'>Cor:</Typography>
+                    {product.color.map(color => {
+                      const isSelected = selectedColor === color
+                      return (
+                        <Stack
+                          p={.5}
+                          mx={.5}
+                          key={color}
+                          minWidth='50px'
+                          borderRadius={1}
+                          alignItems='center'
+                          justifyContent='center'
+                          sx={{ cursor: 'pointer' }}
+                          border='1px solid rgba(0,0,0,.1)'
+                          color={selectedItemsColor(isSelected)}
+                          bgcolor={selectedItemsBgColor(isSelected)}
+                          onClick={() => {
+                            if (isSelected) {
+                              return
+                            }
+                            setSelectedColor(color)
+                          }}
+                        >
+                          <Typography fontSize='13px' variant='body2'>{color}</Typography>
+                        </Stack>
+                      )
+                    })}
+                  </Stack>
+                  {haveColorError && <Typography fontSize={12} color='red' variant='body2'>Obrigatório</Typography>}
+                </>
+              )}
+              {product?.size && (
+                <>
+                  <Stack mt={1} direction='row' alignItems='center'>
+                    <Typography mr={.5} variant='body2'>Tamanho:</Typography>
+                    {product.size.map(size => {
+                      const isSelected = selectedSize === size
+                      return (
+                        <Stack
+                          p={.5}
+                          mx={.5}
+                          key={size}
+                          minWidth='50px'
+                          borderRadius={1}
+                          alignItems='center'
+                          justifyContent='center'
+                          sx={{ cursor: 'pointer' }}
+                          border='1px solid rgba(0,0,0,.1)'
+                          color={selectedItemsColor(isSelected)}
+                          bgcolor={selectedItemsBgColor(isSelected)}
+                          onClick={() => {
+                            if (isSelected) {
+                              return
+                            }
+                            setSelectedSize(size)
+                          }}
+                        >
+                          <Typography fontSize='13px' variant='body2'>{size}</Typography>
+                        </Stack>
+                      )
+                    })}
+                  </Stack>
+                  {haveSizeError && <Typography fontSize={12} color='red' variant='body2'>Obrigatório</Typography>}
+                </>
+              )}
+            </Stack>
+
+            <Stack
+              mt={2}
+              p={1.5}
+              direction='row'
+              borderRadius={2}
+              alignItems='center'
+              onClick={() => addToCart()}
+              justifyContent='space-between'
+              sx={{ opacity: .6, cursor: 'pointer' }}
+            >
+              <Typography variant='button'>Adicionar ao carrinho</Typography>
+              <ShoppingBagOutlined />
+            </Stack>
+
+            <Stack
+              py={2}
+              px={1.5}
+              mt={1.5}
+              direction='row'
+              bgcolor='white'
+              borderRadius={2}
+              alignItems='center'
+              position='relative'
+              justifyContent='space-between'
+              border={`${isDarkMode ? '1.5px' : '1px'} solid transparent`}
+              sx={{
+                background: 'linear-gradient(white, white) padding-box, linear-gradient(80.42deg, #9A00FF 7.33%, #7241FF 51.42%, orange 92.84%) border-box',
+              }}
+              onClick={() => finishOrder()}
+            >
+              <Typography
+                variant='button'
+                sx={{
+                  color: 'transparent',
+                  backgroundClip: 'text',
+                  mozBackgroundClip: 'text',
+                  webkitBackgroundClip: 'text',
+                  backgroundImage: 'linear-gradient(80.42deg, #9A00FF 7.33%, #7241FF 51.42%, orange 92.84%)',
+                }}
+              >
+                Comprar agora
+              </Typography>
+              <SellOutlined sx={{ color: 'orange' }} />
+            </Stack>
           </Stack>
-        )
-      }
+        </Stack>
+      )}
     </Dialog >
   )
 }
